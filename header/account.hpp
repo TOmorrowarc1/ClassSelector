fa0353fb45bbeb47419d811741ba3215080046e8
TOmorrowarc1 <TOmorrow_arc1@sjtu.edu.cn>
2025-08-03 10:44:40 +0800

    feat: handle out info of courses.

--- header/account.hpp
+++ header/account.hpp
@@ -3,7 +3,7 @@
 #include "coreinfo.hpp"
 #include <vector>
 
-struct studentInfo {
+struct studentInfoOut {
   std::string name;
   int32_t grade;
   int32_t class_number;
@@ -13,23 +13,23 @@ struct studentInfo {
 auto createAdminister() -> bool;
 
 // Administer register new account for student;
-auto signUp(const UserSession &session, const studentInfo &info,
+auto signUp(const UserSession &session, const studentInfoOut &info,
             int32_t priviledge) -> bool;
 
 // Administer remove a student from the list.
-auto removeUser(const studentInfo &info) -> bool;
+auto removeUser(const studentInfoOut &info) -> bool;
 
 // Allow the administer get all uid for a class;
-auto fetchUserClass(const studentInfo &class_info) -> std::vector<int32_t>;
+auto fetchUserClass(const studentInfoOut &class_info) -> std::vector<int32_t>;
 
 // Allow the administer get every uid.
-auto fetchUserAll(const studentInfo &class_info) -> std::vector<int32_t>;
+auto fetchUserAll() -> std::vector<int32_t>;
 
 // Everyone has logged in can modify his password.
 auto modifyPassword(const UserSession &session, int32_t new_password) -> bool;
 
 // Users log in.
-auto logIn(const studentInfo &info) -> std::pair<bool, UserSession>;
+auto logIn(const studentInfoOut &info) -> std::pair<bool, UserSession>;
 
 // Users log out.
 auto logOut(const UserSession &session) -> bool;
--- header/courses.hpp
+++ header/courses.hpp
@@ -1,3 +1,29 @@
 #ifndef COURSE_HPP
 #define COURSE_HPP
+#include "coreinfo.hpp"
+
+struct courseInfoOut {
+  std::string name;
+  std::string teacher;
+  int32_t total_number;
+};
+
+auto addCourse(const UserSession &session, const courseInfoOut &new_course)
+    -> bool;
+
+auto modifyCourse(const UserSession &session, const courseInfoOut &new_course)
+    -> bool;
+
+auto launchCourse(const UserSession &session, const std::string &name) -> bool;
+
+auto searchCourse(const UserSession &session, const std::string &name)
+    -> courseInfoOut;
+
+auto selectCourse(const UserSession &session, const std::string &name) -> bool;
+
+auto checkCourse(const UserSession &session, const std::string &name)
+    -> std::pair<bool, std::string>;
+
+auto dropCourse(const UserSession &session, const std::string &name) -> bool;
+
 #endif
