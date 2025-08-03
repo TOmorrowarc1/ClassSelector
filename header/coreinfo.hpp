9351cdda5e59040df63b924a0dcfae01e0ae6662
TOmorrowarc1 <TOmorrow_arc1@sjtu.edu.cn>
2025-08-03 10:20:29 +0800

    feat: set names of all files.

--- header/coreinfo.hpp
+++ header/coreinfo.hpp
@@ -1,10 +1,17 @@
 #ifndef COREINFO_HPP
 #define COREINFO_HPP
 #include <stdint.h>
+#include <string>
 
 struct UserSession {
   int32_t uid_;
   int32_t priviledge_;
 };
+
+extern std::string file_name_logger;
+extern std::string file_name_account;
+extern std::string file_name_course_static;
+extern std::string file_name_course_remain;
+extern std::string file_name_course_enrol;
 
 #endif
---
+++ src/coreinfo.cpp
@@ -0,0 +1,7 @@
+#include "coreinfo.hpp"
+
+std::string file_name_logger = "logger_message";
+std::string file_name_account = "account_info";
+std::string file_name_course_static = "course_info";
+std::string file_name_course_remain = "course_remain";
+std::string file_name_course_enrol = "course_enroller";
