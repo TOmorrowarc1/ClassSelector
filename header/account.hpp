c67c977b8e2ad61b324579a91959592bbdc632e6
TOmorrowarc1 <TOmorrow_arc1@sjtu.edu.cn>
2025-08-03 10:20:57 +0800

    feat: design the interface of account sys.

--- header/account.hpp
+++ header/account.hpp
@@ -1,3 +1,37 @@
 #ifndef ACCOUNT_HPP
 #define ACCOUNT_HPP
+#include "coreinfo.hpp"
+#include <vector>
+
+struct studentInfo {
+  std::string name;
+  int32_t grade;
+  int32_t class_number;
+  int32_t password;
+};
+
+auto createAdminister() -> bool;
+
+// Administer register new account for student;
+auto signUp(const UserSession &session, const studentInfo &info,
+            int32_t priviledge) -> bool;
+
+// Administer remove a student from the list.
+auto removeUser(const studentInfo &info) -> bool;
+
+// Allow the administer get all uid for a class;
+auto fetchUserClass(const studentInfo &class_info) -> std::vector<int32_t>;
+
+// Allow the administer get every uid.
+auto fetchUserAll(const studentInfo &class_info) -> std::vector<int32_t>;
+
+// Everyone has logged in can modify his password.
+auto modifyPassword(const UserSession &session, int32_t new_password) -> bool;
+
+// Users log in.
+auto logIn(const studentInfo &info) -> std::pair<bool, UserSession>;
+
+// Users log out.
+auto logOut(const UserSession &session) -> bool;
+
 #endif
